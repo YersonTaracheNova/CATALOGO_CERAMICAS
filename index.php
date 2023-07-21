1ec0b19183fc9f94a246bfba8e2ad7ba1d393ac1
YersonTaracheNova <55007695+YersonTaracheNova@users.noreply.github.com>
2023-07-21 10:30:41 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>catalogo ceramicas</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <!--libreria de iconos -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <style>
+        body {
+            background-image: linear-gradient(to right, #CBF719, #FF843D);
+            background-repeat: no-repeat;
+            background-size: cover;
+            /* Opcional: ajusta la imagen para cubrir todo el fondo */
+            /* Otras propiedades de fondo como background-position o background-color pueden configurarse aquí */
+        }
+
+        .letratitulo {
+            font-style: italic;
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+    </style>
+</head>
+
+<body class=>
+    <div class="container">
+        <div class="row text-center mt-4">
+            <H2 class="letratitulo">CATALOGO DE CERAMICA</H2>
+        </div>
+        <div class="row mt-5">
+            <h5 class="text-center"><strong>SIGUENOS EN NUESTRAS REDES SOCIALES</strong></h5>
+        </div>
+        <div class="row justify-content-center mt-3">
+            <div class="col-8">
+                <div class="row">
+                    <div class="col-4 text-center">
+                        <a href="https://www.facebook.com/tu_pagina" target="_blank" title="Facebook"><i class="fab fa-facebook" style="font-size: 3rem;"></i></a>
+                    </div>
+                    <div class="col-4 text-center">
+                        <a href="https://www.instagram.com/tu_cuenta" target="_blank" title="Instagram"><i class="fab fa-instagram" style="font-size: 3rem; color:purple"></i></a>
+                    </div>
+                    <!--<div class="col-3 text-center">
+                <a href="https://www.youtube.com/c/tu_canal" target="_blank" title="YouTube"><i class="fab fa-youtube" style="font-size: 3rem;"></i></a>
+            </div>-->
+                    <div class="col-4 text-center">
+                        <a href="https://twitter.com/tu_usuario" target="_blank" title="Twitter"><i class="fab fa-twitter" style="font-size: 3rem;"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <iframe src="pdf/catalogo_ceramica.pdf" width="100%" height="800px"></iframe>
+        </div>
+    </div>
+</body>
+
+</html>
